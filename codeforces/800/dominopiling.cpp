a3ca90499ce7af172d8fab711172ef3326b42e45
MahaDrahman <mahadirahmanlabib50@gmail.com>
2026-01-19 17:39:52 +0600

    50A domino piling solution

---
+++ codeforces/800/dominopiling.cpp
@@ -0,0 +1,16 @@
+/* Practice is the only shortcut */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);  cout.tie(nullptr);
+
+    int n, m; cin >> n >> m;
+    cout << (n * m) / 2 <<endl;
+
+    return 0;
+}
