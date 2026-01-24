efffd3254d7ac594316c8909dc9be9896cdf81cf
MahaDrahman <mahadirahmanlabib50@gmail.com>
2026-01-24 22:42:17 +0600

    617A Elephant solution

---
+++ codeforces/800/elephant.cpp
@@ -0,0 +1,16 @@
+/* Practice is the only shortcut */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);  cout.tie(nullptr);
+
+    double n; cin >> n;
+    cout << ceil(n / 5);
+
+    return 0;
+}
