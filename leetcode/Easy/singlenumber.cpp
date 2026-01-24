2ece3b8faec459c6fdf0177f2e64305506cb5d35
MahaDrahman <mahadirahmanlabib50@gmail.com>
2026-01-25 00:16:12 +0600

    single number solve with xor

--- leetcode/Easy/singlenumber.cpp
+++ leetcode/Easy/singlenumber.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-      sort(nums.begin(), nums.end());
-       vector<int> vnew;
-       for(auto x : nums){
-        if(!vnew.empty() and vnew.back() == x){
-            vnew.pop_back();
-        }else{
-            vnew.push_back(x);
-        }
-       }
-       return vnew[0];
+      int ans = 0;
+      for(int x : nums){
+        ans ^= x;
+      }
+       return ans ;
     }
+   
 };
