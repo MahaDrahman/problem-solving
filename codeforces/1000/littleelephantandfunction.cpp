8aad5aecd022ac376d740c8d80bce9f8a3548f9e
MahaDrahman <mahadirahmanlabib50@gmail.com>
2026-02-21 15:47:51 +0600

    221A little elephant and function solution

---
+++ codeforces/1000/littleelephantandfunction.cpp
@@ -0,0 +1,20 @@
+/* Practice is the only shortcut */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);  cout.tie(nullptr);
+
+    int n; cin >> n;
+    cout << n <<" ";
+    for(int i = 1; i <= n - 1; i++){
+      cout << i << ' ';
+    }
+    cout << endl;
+
+    return 0;
+}
